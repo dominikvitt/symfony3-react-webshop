de8f5b49b0560f179f84c21250c6e66c091edf21
Dominik Vitt <dominik.vitt.hr@gmail.com>
2019-06-12 13:07:21 +0200

    Add webpack.config.js file

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+
+    .addEntry('app', './assets/js/app.js')
+
+    // ...
+;
+
+// ...
