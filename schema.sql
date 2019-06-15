e237561cb28157d7dc3341822d3ac3d2632bab73
Dominik Vitt <dominik.vitt.hr@gmail.com>
2019-06-15 18:14:26 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -32,30 +32,47 @@ CREATE TABLE IF NOT EXISTS `language` (
 /*!40000 ALTER TABLE `language` DISABLE KEYS */;
 /*!40000 ALTER TABLE `language` ENABLE KEYS */;
 
+-- Dumping structure for table symfony3-webshop.manufacturer
+CREATE TABLE IF NOT EXISTS `manufacturer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin7;
+
+-- Dumping data for table symfony3-webshop.manufacturer: ~2 rows (approximately)
+/*!40000 ALTER TABLE `manufacturer` DISABLE KEYS */;
+INSERT INTO `manufacturer` (`id`, `name`) VALUES
+	(1, 'Manufacturer #1'),
+	(2, 'Manufacturer #2'),
+	(3, 'Manufacturer #3');
+/*!40000 ALTER TABLE `manufacturer` ENABLE KEYS */;
+
 -- Dumping structure for table symfony3-webshop.product
 CREATE TABLE IF NOT EXISTS `product` (
   `product_id` int(11) NOT NULL AUTO_INCREMENT,
   `model` varchar(64) NOT NULL,
   `sku` varchar(64) NOT NULL,
-  `quantity` int(4) NOT NULL DEFAULT '0',
+  `quantity` int(4) NOT NULL,
   `stock_status_id` int(11) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
   `manufacturer_id` int(11) NOT NULL,
   `shipping` int(11) NOT NULL,
-  `price` decimal(15,4) NOT NULL DEFAULT '0.0000',
+  `price` decimal(15,4) NOT NULL,
   `tax_class_id` int(11) NOT NULL,
   `date_available` datetime DEFAULT NULL,
   `minimum` int(11) NOT NULL DEFAULT '1',
   `sort_order` int(11) NOT NULL DEFAULT '0',
   `status` int(11) NOT NULL DEFAULT '0',
   `viewed` int(5) NOT NULL DEFAULT '0',
-  `date_added` datetime NOT NULL,
-  `date_modified` datetime NOT NULL,
+  `date_added` datetime DEFAULT NULL,
+  `date_modified` datetime DEFAULT NULL,
   PRIMARY KEY (`product_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=50 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=53 DEFAULT CHARSET=utf8;
 
--- Dumping data for table symfony3-webshop.product: 0 rows
+-- Dumping data for table symfony3-webshop.product: 1 rows
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` (`product_id`, `model`, `sku`, `quantity`, `stock_status_id`, `image`, `manufacturer_id`, `shipping`, `price`, `tax_class_id`, `date_available`, `minimum`, `sort_order`, `status`, `viewed`, `date_added`, `date_modified`) VALUES
+	(52, '1', '2', 3, 1, 'e6b7d70558d6594a43335f14feaa02f4.jpeg', 1, 1, 2.0000, 1, NULL, 1, 0, 1, 0, NULL, NULL);
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 
 -- Dumping structure for table symfony3-webshop.product_description
@@ -72,6 +89,21 @@ CREATE TABLE IF NOT EXISTS `product_description` (
 -- Dumping data for table symfony3-webshop.product_description: 0 rows
 /*!40000 ALTER TABLE `product_description` DISABLE KEYS */;
 /*!40000 ALTER TABLE `product_description` ENABLE KEYS */;
+
+-- Dumping structure for table symfony3-webshop.tax_class
+CREATE TABLE IF NOT EXISTS `tax_class` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '0',
+  `percentage` decimal(4,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin7;
+
+-- Dumping data for table symfony3-webshop.tax_class: ~2 rows (approximately)
+/*!40000 ALTER TABLE `tax_class` DISABLE KEYS */;
+INSERT INTO `tax_class` (`id`, `name`, `percentage`) VALUES
+	(1, 'Regular Tax', 10.00),
+	(2, 'Extra Tax', 20.00);
+/*!40000 ALTER TABLE `tax_class` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
